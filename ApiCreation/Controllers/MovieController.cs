49842afe78f169fcc40d8f1e2b2b10dd1672edf4
Robert Smidt <rsmidt909@hotmail.com>
2019-06-11 14:05:40 -0500

    Added in get all for get api to get the a list of movies

---
+++ ApiCreation/Controllers/MovieController.cs
@@ -0,0 +1,67 @@
+﻿using ApiCreation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
+
+namespace ApiCreation.Controllers
+{
+    public class MovieController : ApiController
+    {
+
+        public ApplicationDbContext db;
+            public MovieController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+
+        // GET api/<controller>
+        public IEnumerable<Movie> GetAll()
+        {
+            List<Movie> thing = db.Movies.ToList();
+            return thing;
+        }
+
+        // GET api/<controller>/5
+        public Movie Get(int id)
+        {
+            //var thing2 = db.Movies.Find(id);
+            //return thing2;
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]Movie value)
+        {
+
+            //if(value == null)
+            //{
+            //    throw new Exception("That aint right punk");
+            //}
+            //db.Movies.Add(value);
+            //db.SaveChanges();
+
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]Movie value)
+        {
+            //var thing1 = db.Movies.Find(id);
+            //thing1 = value;
+            //db.SaveChanges();
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+            //var thing1337 = db.Movies.Find(id);
+            //db.Movies.Remove(thing1337);
+            //db.SaveChanges();
+
+        }
+    }
+}
--- ApiCreation/Migrations/Configuration.cs
+++ ApiCreation/Migrations/Configuration.cs
@@ -15,19 +15,19 @@ namespace ApiCreation.Migrations
             db = new ApplicationDbContext();
         }
 
-        protected override void Seed(ApiCreation.Models.ApplicationDbContext context)
+        protected override void Seed(ApiCreation.Models.ApplicationDbContext db)
         {
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
-            context.Movies.AddOrUpdate(
-    new Models.Movies { Title = "The Departed", Genre = "Drama", DirectorName = "Martin Scorsese" },
-    new Models.Movies { Title = "The Dark Knight", Genre = "Drama", DirectorName = "Christopher Nolan" },
-    new Models.Movies { Title = "Inception", Genre = "Drama", DirectorName = "Christopher Nolan" },
-    new Models.Movies { Title = "Pineapple Express", Genre = "Comedy", DirectorName = "David Gordon Green" },
-    new Models.Movies { Title = "Die Hard", Genre = "Action", DirectorName = "John McTiernan" }
-);
+    //        db.Movies.AddOrUpdate(
+    //new Models.Movie { Title = "The Departed", Genre = "Drama", DirectorName = "Martin Scorsese" },
+    //new Models.Movie { Title = "The Dark Knight", Genre = "Drama", DirectorName = "Christopher Nolan" },
+    //new Models.Movie { Title = "Inception", Genre = "Drama", DirectorName = "Christopher Nolan" },
+    //new Models.Movie { Title = "Pineapple Express", Genre = "Comedy", DirectorName = "David Gordon Green" },
+    //new Models.Movie { Title = "Die Hard", Genre = "Action", DirectorName = "John McTiernan" }
+//);
         }
     }
 }
